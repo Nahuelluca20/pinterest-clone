1d2ba58e0267308b50c8f47386a84ea4b8ee7d9c
Nahuel Luca <nahuelluca20@gmail.com>
2023-02-07 21:28:50 -0300

    fix: url

--- app/services/testing.service.js
+++ app/services/testing.service.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export const TestingService = () => {
-  return axios.get("https://pinterest-clone-api.vercel.app//api/card");
+  return axios.get("https://pinterest-clone-api.vercel.app/api/card");
 };
 
 export const GetCardById = (id) => {
-  return axios.get(`https://pinterest-clone-api.vercel.app//api/card/${id}`);
+  return axios.get(`https://pinterest-clone-api.vercel.app/api/card/${id}`);
 };
 
 export const AddCommentService = (id, comment) => {
-  return axios.patch(`https://pinterest-clone-api.vercel.app//api/card/${id}`, {
+  return axios.patch(`https://pinterest-clone-api.vercel.app/api/card/${id}`, {
     comments: comment,
   });
 };
 
 export const PostPin = (img, title) => {
-  return axios.post(`https://pinterest-clone-api.vercel.app//api/card`, {
+  return axios.post(`https://pinterest-clone-api.vercel.app/api/card`, {
     img: img,
     link: "https://www.tumblr.com/aes-tivate",
     title: title,
